557e69f6153e7d973254f3a66f5a1124d4283cae
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-10-21 23:39:30 +0600

    common include added

--- app/src/main/java/com/frskynet/as_deliveryreport/NonDeliveryMan.java
+++ app/src/main/java/com/frskynet/as_deliveryreport/NonDeliveryMan.java
@@ -3,10 +3,18 @@ package com.frskynet.as_deliveryreport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class NonDeliveryMan extends Activity {
 
