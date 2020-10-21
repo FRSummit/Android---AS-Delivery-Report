5ad2dcd1cf87017331bdf14819e0112f0c5e2041
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-10-21 12:53:05 +0600

    anim added

--- app/src/main/java/com/frskynet/as_deliveryreport/MainActivity.java
+++ app/src/main/java/com/frskynet/as_deliveryreport/MainActivity.java
@@ -3,7 +3,11 @@ package com.frskynet.as_deliveryreport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +15,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changeActivity();
+    }
+
+    private void changeActivity() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
     }
 }
