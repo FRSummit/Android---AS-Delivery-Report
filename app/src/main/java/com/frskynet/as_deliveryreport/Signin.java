3385e558e02cb73bccbc0306f7a20d9cfe90da6a
frsummit@simplexhub.com <www.frsummit1@gmail.com>
2020-10-22 18:28:28 +0600

    Logo updated and login class and layout done

--- app/src/main/java/com/frskynet/as_deliveryreport/Signin.java
+++ app/src/main/java/com/frskynet/as_deliveryreport/Signin.java
@@ -3,7 +3,10 @@ package com.frskynet.as_deliveryreport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class Signin extends Activity {
 
@@ -11,5 +14,10 @@ public class Signin extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+    }
+
+    public void visitWebsiteHandler(View view) {
+        Uri uri = Uri.parse("http://amarshasroy.com");
+        startActivity(new Intent(Intent.ACTION_VIEW, uri));
     }
 }
